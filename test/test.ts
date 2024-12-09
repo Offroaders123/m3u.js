46c535b35922cfc7e86f3a52e4df4dc93f352014
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-12-09 13:49:13 -0800

    Should to Expect
    Personally I like this test setup more because it explicitly uses parts of the testing library, without needing to modify the environment to add things to the global prototype for Object. I like the concept of how that works a lot, but I don't want to do something accidentally in the event of that.
    
    https://www.chaijs.com/api/bdd/
    https://stackoverflow.com/questions/21396524/what-is-the-difference-between-assert-expect-and-should-in-chai

--- test/test.ts
+++ test/test.ts
@@ -3,10 +3,8 @@
  */
 
 import { describe, it } from 'node:test';
-import chai = require('chai');
 import fs = require('fs');
-import { should, expect } from 'chai';
-should();
+import { expect } from 'chai';
 
 import m3u = require('../src/index.js');
 
@@ -17,66 +15,66 @@ describe("Parsing m3u", function() {
     //console.log("Parsed: "+util.inspect(parsed));
 
     it("Should be parsed to object", function(){
-        parsed.should.be.an('object');
-        parsed.should.have.ownProperty('header');
+        expect(parsed).to.be.an('object');
+        expect(parsed).to.have.ownProperty('header');
     });
 
     var header: m3u.Params = parsed.header;
 
     it("Should have header with 2 params", function(){
-        Object.keys(header).length.should.equal(2);
-        header.should.be.an('object');
-        header.should.have.ownProperty('param1');
-        header.should.have.ownProperty('param2');
-        header['param1']!.should.equal('val1');
-        header['param2']!.should.equal('val2 val3');
+        expect(Object.keys(header).length).to.equal(2);
+        expect(header).to.be.an('object');
+        expect(header).to.have.ownProperty('param1');
+        expect(header).to.have.ownProperty('param2');
+        expect(header['param1']!).to.equal('val1');
+        expect(header['param2']!).to.equal('val2 val3');
     });
 
     var tracks: m3u.Track[] = parsed.tracks;
 
     it("Should have 3 tracks", function() {
-        tracks.should.be.an('array');
-        tracks.length.should.equal(3);
+        expect(tracks).to.be.an('array');
+        expect(tracks.length).to.equal(3);
     });
 
     var track1: m3u.Track = tracks[0]!;
 
     it("Should parse track 1", function() {
-        track1.should.be.an('object');
-        track1.title.should.equal('Test');
-        track1.length.should.equal(0);
-        track1.params.should.be.an('object');
+        expect(track1).to.be.an('object');
+        expect(track1.title).to.equal('Test');
+        expect(track1.length).to.equal(0);
+        expect(track1.params).to.be.an('object');
     });
 
     it("Should parse track params", function() {
         var params: m3u.Params = track1.params;
-        params.should.be.an('object');
-        Object.keys(params).length.should.equal(2);
-        params.should.have.ownProperty('p1');
-        params.should.have.ownProperty('p2');
-        params['p1']!.should.equal('v1 t');
-        params['p2']!.should.equal('v2');
+        expect(params).to.be.an('object');
+        expect(Object.keys(params).length).to.equal(2);
+        expect(params).to.have.ownProperty('p1');
+        expect(params).to.have.ownProperty('p2');
+        expect(params['p1']!).to.equal('v1 t');
+        expect(params['p2']!).to.equal('v2');
     });
 
     it("Should parse track 2", function() {
         var track2: m3u.Track = tracks[1]!;
 
-        track2.should.be.an('object');
-        track2.length.should.equal(-1);
-        track2.file.should.equal('http://url2');
+        expect(track2).to.be.an('object');
+        expect(track2.length).to.equal(-1);
+        expect(track2.file).to.equal('http://url2');
     });
 
     it("Should parse track length", function() {
         var track3: m3u.Track = tracks[2]!;
 
-        track3.should.be.an('object');
-        track3.length.should.equal(500);
+        expect(track3).to.be.an('object');
+        expect(track3.length).to.equal(500);
     });
 });
 
 describe("Formatting test", function(){
     it("Should format global params", function(){
         var formatted: string = m3u.format({header: {param1: 'val1', 'param2': 'val2'}, tracks: []});
-        formatted.should.equal('#EXTM3U param1="val1" param2="val2"\n');
+        expect(formatted).to.equal('#EXTM3U param1="val1" param2="val2"\n');
     });
 });
